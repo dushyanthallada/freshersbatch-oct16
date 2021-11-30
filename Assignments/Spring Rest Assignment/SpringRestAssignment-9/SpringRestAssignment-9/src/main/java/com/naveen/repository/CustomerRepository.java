67839b6e88f5215bf85b419617f8b66fe25808aa
Allada <dushyanth-naidu.allada@capgemini.com>
2021-11-30 12:49:32 +0530

    added rest

--- Assignments/Spring Rest Assignment/SpringRestAssignment-9/SpringRestAssignment-9/src/main/java/com/naveen/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.naveen.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.naveen.model.Customer;
-
-public interface CustomerRepository extends MongoRepository<Customer,Integer>{
-
-}
